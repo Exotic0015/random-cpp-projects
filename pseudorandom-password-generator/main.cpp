41640a52d57c1cd9222629eb90b04466fdeeaaa7
Exotic0015 <jalokim541515@gmail.com>
2022-04-14 19:19:54 +0200

    Rename a temporary variable name to a more appropriate one

--- pseudorandom-password-generator/main.cpp
+++ pseudorandom-password-generator/main.cpp
@@ -29,7 +29,7 @@ int main() {
 
     // Initialize a pseudorandom rng with a range between 48 ("0" in ASCII) and 122 ("z" in ASCII)
     random_device rng;
-    default_random_engine aa(rng());
+    default_random_engine engine(rng());
     uniform_int_distribution uniform(48, 122);
 
     // Initialize a passphrase string variable
@@ -37,7 +37,7 @@ int main() {
 
     for (size_t i{}; i < count; i++) {
         // Generate a random character
-        char random_char = (char) uniform(aa);
+        char random_char = (char) uniform(engine);
 
         // If the character is in the blacklist, decrement the iterator and start a new iteration
         if (find(begin(blacklist), end(blacklist), random_char) != end(blacklist)) {
