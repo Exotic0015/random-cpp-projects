90102d879b28ebcaa556bfd20f16663a56ebbdbd
Exotic <mpalys7274@gmail.com>
2022-06-03 01:28:44 +0200

    Remove trailing spaces

--- text-converter/functions.hpp
+++ text-converter/functions.hpp
@@ -5,39 +5,43 @@
 #include <bitset>
 #include <iomanip>
 
-std::string string_to_decimal(std::string &rawString) noexcept {
+std::string string_to_decimal(const std::string& rawString) noexcept {
     std::string convertedString{};
     for (char v: rawString) {
         convertedString += std::to_string(v) + ' ';
     }
+    convertedString.pop_back(); // Remove the trailing space
     return convertedString;
 }
 
-std::string string_to_binary(std::string &rawString) noexcept {
+std::string string_to_binary(const std::string& rawString) noexcept {
     std::string convertedString{};
     for (char v: rawString) {
         convertedString += std::bitset<8>(v).to_string() + ' ';
     }
+    convertedString.pop_back(); // Remove the trailing space
     return convertedString;
 }
 
-std::string string_to_hex(std::string &rawString) noexcept {
+std::string string_to_hex(const std::string& rawString) noexcept {
     std::string convertedString{};
     for (char v: rawString) {
         std::stringstream stream{};
         stream << std::hex << (int) v;
         convertedString += stream.str() + ' ';
     }
+    convertedString.pop_back(); // Remove the trailing space
     return convertedString;
 }
 
-std::string string_to_oct(std::string &rawString) noexcept {
+std::string string_to_oct(const std::string& rawString) noexcept {
     std::string convertedString{};
     for (char v: rawString) {
         std::stringstream stream{};
         stream << std::oct << (int) v;
         convertedString += stream.str() + ' ';
     }
+    convertedString.pop_back(); // Remove the trailing space
     return convertedString;
 }
 
